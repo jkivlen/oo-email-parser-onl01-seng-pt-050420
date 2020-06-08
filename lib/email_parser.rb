db2d18a5835987d5dbc1b61702761de7ac78fce3
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-08 14:31:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,17 +4,7 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-attr_accessor :emails 
 
-def initialize(emails)
-  @emails = emails
-end 
-
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
 
 end
   
