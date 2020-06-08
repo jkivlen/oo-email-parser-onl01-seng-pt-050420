9afd54ab57c40c528d3e1f61a3102478d08c9beb
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-08 13:44:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -19,11 +19,4 @@ class EmailParser
     
 end
   
-  emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
   
